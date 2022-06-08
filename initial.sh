7881fa82c7c3537178206f65c638e9d38bc7fee3
utnightwind <xuhuiluo@gmail.com>
2022-06-08 15:22:18 -0500

    basic caluclator

---
+++ initial.sh
@@ -0,0 +1,7 @@
+echo "# algorithm_notes" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/utnightwind/algorithm_notes.git
+git push -u origin main
